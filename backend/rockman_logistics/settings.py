4991bda4cf3929b74b241f550f2edcd440e313bb
Emmanuel Klutse <nuelklus@gmail.com>
2026-02-19 16:02:50 +0000

    Add STATIC_ROOT setting to fix collectstatic error

--- backend/rockman_logistics/settings.py
+++ backend/rockman_logistics/settings.py
@@ -133,15 +133,14 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/6.0/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # REST Framework configuration
 REST_FRAMEWORK = {
